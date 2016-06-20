4627eb147e8de86d9e3e435097b550dbce6449e0
Jannis Widderich <j.widderich@bichinger.de>
2016-06-20 15:58:36 +0200

    Add spree_i18n/locale to required files

--- lib/spree_i18n.rb
+++ lib/spree_i18n.rb
@@ -3,5 +3,10 @@ require 'rails-i18n'
 require 'spree_core'
 require 'spree_i18n/engine'
 require 'spree_i18n/version'
+# locale.rb needs to be explicitly required in environments with `config.eager_load = true`,
+# otherwise a `NameError: uninitialized constant SpreeI18n::LocaleHelper::Locale` will be raised.
+# I'm not sure why other modules (like Configuration or ControllerLocaleHelper) seem to
+# work. It might very well be that this is not the correct way to solve this issue.
+require 'spree_i18n/locale'
 require 'spree/i18n_utils'
 require 'coffee_script'
